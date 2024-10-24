6cc8ce23c074257f053ae7157353628c2cca7d04
Excellence15 <excellenceadedotun2023@gmail.com>
2024-10-24 08:52:23 +0100

    files added

---
+++ calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+int derivative,main, power;
+printf("Enter main: ");
+scanf("%d", &main);
+printf("Enter power: ");
+scanf("%d", &power);
+derivative = power*(main(^power-1);
+printf("derivative is %d" ,derivative);
+}
